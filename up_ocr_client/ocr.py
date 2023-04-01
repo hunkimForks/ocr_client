fd7061ed10eb806278085550dd9cee34ec38d576
Lucy Park <me@lucypark.kr>
2023-04-01 19:57:44 +0900

    refactor: Pretty format

--- up_ocr_client/ocr.py
+++ up_ocr_client/ocr.py
@@ -10,6 +10,23 @@ def get_confidence_score(data):
     return result.get("document_confidence")
 
 
+def process_image(image):
+    # If input is a byte string, return as-is
+    if isinstance(image, bytes):
+        return image
+
+    # If input is a filename, read
+    elif isinstance(image, str) and os.path.isfile(image):
+        return open(image, "rb").read()
+
+    # Check if input is a base64 string, convert to bytes
+    else:
+        try:
+            return base64.b64decode(image.split(',')[-1])
+        except Exception as e:
+            raise ValueError(f'Invalid input: image must be a filename, byte string, or base64 string: {e}')
+
+
 def select_target_from_data(data, target):
     if target == 'text':
         words = json.loads(data["ocrResult"]["result"])["words"].values()
@@ -26,20 +43,6 @@ def select_target_from_data(data, target):
         return data
 
 
-def process_image(image):
-    # Input is a byte string
-    if isinstance(image, bytes):
-        return image
-    # Input is a filename
-    elif isinstance(image, str) and os.path.isfile(image):
-        return open(image, "rb")
-    # Input is a base64 string
-    elif isinstance(image, str) and image.startswith('data:image/'):
-        return base64.b64decode(image.split(',')[1])
-    else:
-        raise ValueError('Invalid input: image must be a filename, byte string, or base64 string')
-
-
 class UpOCRClient:
     def __init__(self, url: str, api_key: str, timeout=10, log_level="INFO"):
         # Set variables
@@ -54,17 +57,14 @@ class UpOCRClient:
         self.logger.setLevel(level=self.log_level)
 
     def request(self, image, target=None, confidence_threshold=0.95) -> dict:
-        # Process image
         try:
+            # Process image
             files = {"image": process_image(image)}
-        except Exception as e:
-            self.logger.error("Failed during processing image: " + str(e))
 
-        # Get response
-        response = requests.post(self.url, headers=self.headers, files=files, timeout=self.timeout)
+            # Get response
+            response = requests.post(self.url, headers=self.headers, files=files, timeout=self.timeout)
 
-        # Check confidence score
-        try:
+            # Check confidence score
             data = response.json()
             confidence = get_confidence_score(data)
             self.logger.debug(f"Confidence: {confidence}")
@@ -72,10 +72,12 @@ class UpOCRClient:
                 return select_target_from_data(data, target)
             else:
                 raise ValueError(f"Confidence insufficient: {confidence} < {confidence_threshold}")
+
         # Raise request error
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Upstage API request exception: {e}")
             return None
+
         # Raise any other error
         except Exception as e:
             self.logger.error(f"Upstage API failed: {e}")
