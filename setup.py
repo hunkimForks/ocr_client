9533e862e108a4b31fcb8bb972fa08975e57c084
Lucy Park <me@lucypark.kr>
2023-04-16 17:47:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="upstage",
-    version="0.2.0-rc1",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "requests",
